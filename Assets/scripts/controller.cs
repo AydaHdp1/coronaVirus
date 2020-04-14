02db3c2ff02e9eb847ec0c61c4f553ab770203ed
Ayda <oneayda@gmail.com>
2020-04-14 13:31:52 +0430

    my message

---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+	public float pacSpeed;
+	Vector3 pacPosition;
+	// Use this for initialization
+	void Start() {
+		pacPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		pacPosition.x += Input.GetAxis("Horizontal") * pacSpeed * Time.deltaTime;
+		pacPosition.y += Input.GetAxis("Vertical") * pacSpeed * Time.deltaTime;
+		transform.position = pacPosition;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "enemy") {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/enemy_move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_move : MonoBehaviour {
+    public float speed = 40f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // transform.Translate(new Vector3(1, 0, 0)*speed*Time.deltaTime);
+        transform.Translate(new Vector3(0, 1, 0)*speed*Time.deltaTime);
+	}
+}
